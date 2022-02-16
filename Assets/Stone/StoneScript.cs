8ae8c4ab2e52e93af43f6199c584df1302adb063
Elias Kramer <67228319+EliasKramer@users.noreply.github.com>
2022-02-16 22:04:02 +0100

    predictions try

--- Assets/Stone/StoneScript.cs
+++ Assets/Stone/StoneScript.cs
@@ -16,6 +16,9 @@ public class StoneScript : MonoBehaviour
     //the attached rigidbody of the stone
     private Rigidbody2D _rb;
 
+    private Vector2 _lastPos;
+    private Vector2 _predictedPos;
+    private Vector2 _lastRememberedPos;
     private float criticalSpeed = 5f;
     void Start()
     {
@@ -24,10 +27,36 @@ public class StoneScript : MonoBehaviour
         _platform = GameObject.Find("MetaData").GetComponent<MetaDataScript>().Platform;
         _filter.NoFilter();
         _rb = GetComponent<Rigidbody2D>();
+        _lastPos = transform.position;
+        _predictedPos = transform.position;
+        _predictedPos = transform.position;
     }
-
     void FixedUpdate()
     {
+        Vector2 currPos = transform.position;
+
+        //Debug.DrawLine(_lastRememberedPos, _predictedPos, Color.blue, 5f);
+
+
+        _lastRememberedPos = _predictedPos;
+        _predictedPos = currPos + _rb.velocity * Time.deltaTime;
+        _lastPos = currPos;
+
+        float radiusCircle = 0.2f;
+        Collider2D[] overlappingCollidersCheckBefore =  Physics2D.OverlapCircleAll(_predictedPos, radiusCircle);
+
+        bool doesContainPlatform = false;
+        foreach(Collider2D curr in overlappingCollidersCheckBefore)
+        {
+            if(curr == _platform)
+            {
+                doesContainPlatform = true;
+            }
+        }
+
+        bool didBugBack = false;
+
+
         if (_rb.velocity.magnitude > criticalSpeed)
         {
             List<Collider2D> overlapping = new List<Collider2D>();
@@ -44,11 +73,22 @@ public class StoneScript : MonoBehaviour
 
                 transform.position = _lastSafePosition;
                 _rb.velocity = velocityBefore.normalized * criticalSpeed;
+                didBugBack = true;
             }
             else
             {
                 _lastSafePosition = transform.position;
             }
+        }
+        if (doesContainPlatform && didBugBack)
+        {
+            Debug.Log($"Next pos will be in wall - circle prediction with {radiusCircle}");
+            Debug.DrawLine(_predictedPos, (_predictedPos - currPos).normalized * radiusCircle, Color.magenta, 5f); // prototype. fuck you and see you tomorrow
+        }
+
+        if (_platform.OverlapPoint(_predictedPos) && didBugBack)
+        {
+            Debug.Log("Next pos will be in wall - point prediction");
         }
     }
 }
